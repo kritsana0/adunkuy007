fe6674e337104be0485470f5788de3105ff6a4df
janjira012 <s5830213012@phuket.psu.ac.th>
2018-04-10 17:17:10 +0700

    kwai dun commit

---
+++ adunkwaikuy/src/adunkwaikuy/Adunkwaikuy.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package adunkwaikuy;
+
+
+public class Adunkwaikuy {
+
+    public static void main(String[] args) {
+    System.out.println("Kwai Adun & Khan");
+    }
+    
+}
